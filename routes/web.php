39637dee3927c6f7eade65bbb6657a6d382a3932
dbur1683 <mequanit1683@gmail.com>
2022-05-15 18:53:07 +0100

    product commit

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,8 @@ use App\Http\controllers\categoryController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/category/register',[categoryController::class,'register'])->name('category/register');
-Route::post('/category/register',[categoryController::class,'store']);
-Route::get('/category/list', [categoryController::class, 'get_all'])->name('category/list');
-Route::get('/category/edit/{id}', [categoryController::class, 'edit']);
-Route::post('/category/update',[categoryController::class,'update'])->name('category/update')->name('category/update');
-Route::get('/category/delete/{id}', [categoryController::class, 'delete']);
-Route::get('/category/search/{id}', [categoryController::class, 'search'])->name('category/search');
-/*
-Route::get('/product/list', [ProductController::class, 'get_all'])->name('product/list');
-Route::get('/product/edit/{id}', [ProductController::class, 'edit']);
-Route::post('/product/update',[ProductController::class,'update'])->name('product/update');
-Route::get('/product/delete/{id}', [ProductController::class, 'delete']);
-Route::get('/product/search/{id}', [ProductController::class, 'search'])->name('product/search');
-Route::get('/product/register',[productController::class,'register'])->name('product/register');
-Route::post('/product/register',[ProductController::class,'store']);
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
+
+
 
